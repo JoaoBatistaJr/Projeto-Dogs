2b3617ad9e55f874f26aeba00933270b5740bf1a
JoãoB <joaob.dev@gmail.com>
2024-01-05 22:32:18 -0300

    Aula: comment_post

--- src/Components/Feed/Feed.jsx
+++ src/Components/Feed/Feed.jsx
@@ -1,14 +1,16 @@
-import React from "react";
-import FeedModal from "./FeedModal";
-import FeedPhotos from "./FeedPhotos";
+import React from 'react';
+import FeedModal from './FeedModal';
+import FeedPhotos from './FeedPhotos';
 
 const Feed = () => {
   const [modalPhoto, setModalPhoto] = React.useState(null);
 
   return (
     <div>
-      {modalPhoto && <FeedModal photo={modalPhoto} />}
-      <FeedPhotos setModalPhoto={setModalPhoto}/>
+      {modalPhoto && (
+        <FeedModal photo={modalPhoto} setModalPhoto={setModalPhoto} />
+      )}
+      <FeedPhotos setModalPhoto={setModalPhoto} />
     </div>
   );
 };
--- src/Components/Feed/FeedModal.jsx
+++ src/Components/Feed/FeedModal.jsx
@@ -1,26 +1,30 @@
-import React from 'react'
-import styles from './FeedModal.module.css'
-import useFetch from '../../Hooks/useFetch'
-import Error from '../Header/Error'
-import Loading from '../Helper/Loading'
+import React from 'react';
+import styles from './FeedModal.module.css';
+import useFetch from '../../Hooks/useFetch';
+import Error from '../Helper/Error';
+import Loading from '../Helper/Loading';
 import { PHOTO_GET } from '../../Api';
-import PhotoContent from '../Photo/PhotoContent'
+import PhotoContent from '../Photo/PhotoContent';
 
-const FeedModal = ({photo}) => {
-  const {data, error, loading, request} = useFetch();
+const FeedModal = ({ photo, setModalPhoto }) => {
+  const { data, error, loading, request } = useFetch();
 
   React.useEffect(() => {
-    const {url, options} = PHOTO_GET (photo.id);
+    const { url, options } = PHOTO_GET(photo.id);
     request(url, options);
   }, [photo, request]);
 
-  return (
-    <div className={styles.modal}>
-      {error && <Error error={error}/>}
-      {loading && <Loading/>}
-      {data && <PhotoContent data={data}/>}
-    </div>
-  )
-}
+  function handleOutsideClick(event) {
+    if (event.target === event.currentTarget) setModalPhoto(null);
+  }
 
-export default FeedModal
+  return (
+    <div className={styles.modal} onClick={handleOutsideClick}>
+      {error && <Error error={error} />}
+      {loading && <Loading />}
+      {data && <PhotoContent data={data} />}
+    </div>
+  );
+};
+
+export default FeedModal;
